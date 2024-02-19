08cd68c10a804c94df2a75c0cf6627d9f11ff2aa
Yass Almardoud <yaseno2186ss@gmail.com>
2024-02-19 11:29:07 +0100

    #83 Added IndentHelper and IndentHelperTest

---
+++ rule/src/main/java/com/buschmais/jqassistant/core/rule/impl/reader/IndentHelper.java
@@ -0,0 +1,122 @@
+package com.buschmais.jqassistant.core.rule.impl.reader;
+
+public class IndentHelper {
+    /**
+     * This class make the String more effective and understandable. by removing the extra spaces,lines and tabs.
+     * even tho , there is some exceptions where if the tabs are not in order the whole class won't work!
+     * NOTE : Check the TextUtilTest.java to see the examples
+     *
+     * @param text
+     * @return edited text without the unused whitespaces
+     */
+    public static String removeIndent(String text) {
+
+        String textWithoutEmptyLines = removeEmptyLines(text);
+        String[] lines = textWithoutEmptyLines.split("\\n");
+        if (text.isBlank()) {
+            return textWithoutEmptyLines;
+        }
+        int indent = getIndent(lines);
+        return removeIndent(lines, indent);
+    }
+
+    /**
+     * Removes the specified indentation from each line in the provided array of lines. If it's needed.
+     * Removes the trailing and leading spaces from each lines
+     *
+     * @param lines  An array of strings representing lines of text
+     * @param indent The number of leading whitespace characters to remove from each line
+     * @return The text with indentation removed
+     */
+
+    public static String removeIndent(String[] lines, int indent) {
+        StringBuilder resultBuilder = new StringBuilder();
+        // We make a loop here to print the text LINE by LINE. it makes it easier to edit and change!
+        for (int i = 0; i < lines.length; i++) {
+            // We replace all trailing whitespaces from each line. to make it simpler!
+            String line = lines[i].replaceAll("\\s+$", "");
+            if (!line.isBlank()) {
+                resultBuilder.append(line.substring(indent));
+            } else {
+                resultBuilder.append(lines[i]);
+            }
+            if (i < lines.length - 1) {
+                resultBuilder.append("\n");
+            }
+        }
+        return resultBuilder.toString();
+
+    }
+
+    /**
+     * In this method we read the text line by line vertically (in columns) . specially to read  spaces or tabs.
+     * with giving the spaces and tabs a value we can just do anything with it (example : shifting the text to the left,
+     * so we remove the extra leading white spaces from each line equally)
+     * with reading the lines we use Chars. so we can with chars easily move it and match it.
+     * about matching  it, we use previousChars and currentChar. to see if the spaces in all lines are in same
+     * place/position. with that we can delete it easily.
+     *
+     * @param lines
+     * @return the count which is currentColumn.
+     */
+
+    public static int getIndent(String[] lines) {
+        int currentColumn = 0;
+        while (true) {
+            String prevChar = null;
+            for (String line : lines) {
+                if (!line.isBlank()) {
+                    if (currentColumn == line.length()) {
+                        return currentColumn;
+                    }
+                    String currentChar = line.substring(currentColumn, currentColumn + 1);
+                    if (!currentChar.isBlank() || (prevChar != null && !prevChar.equals(currentChar))) {
+                        return currentColumn;
+                    }
+                    prevChar = currentChar;
+                }
+            }
+            currentColumn++;
+        }
+
+    }
+
+    /**
+     * This is method it's works by reading  the text from the top then from the bottom by each line
+     * and see where is the text. if there is a empty lines before or after the text , then this method going to remove it.
+     *
+     * @param text
+     * @return text without trailing and leading  empty lines
+     */
+
+    public static String removeEmptyLines(String text) {
+        String[] lines = text.split("\\n");
+
+        // read the text from the top and give it a value, so we can print from this value.
+        int startIndex = 0;
+        for (int i = 0; i < lines.length; i++) {
+            if (!lines[i].isBlank()) {
+                startIndex = i;
+                break;
+            }
+        }
+
+        // opposite to the last reader , this reads the text from the bottom to top, also give it a value.
+        int lastIndex = lines.length - 1;
+        for (int i = lines.length - 1; i >= 0; i--) {
+            if (!lines[i].isBlank()) {
+                lastIndex = i;
+                break;
+            }
+        }
+        // Here we print the text which is between startIndex and lastIndex, and with that we lose the empty leading trailing lines.
+        StringBuilder resultBuilder = new StringBuilder();
+        for (int i = startIndex; i <= lastIndex; i++) {
+            resultBuilder.append(lines[i]);
+            if (i < lastIndex) {
+                resultBuilder.append("\n");
+            }
+        }
+        return resultBuilder.toString();
+    }
+}
---
+++ rule/src/test/java/com/buschmais/jqassistant/core/rule/impl/reader/IndentHelperTest.java
@@ -0,0 +1,105 @@
+package com.buschmais.jqassistant.core.rule.impl.reader;
+
+import org.junit.jupiter.api.Test;
+
+import static com.buschmais.jqassistant.core.rule.impl.reader.IndentHelper.removeIndent;
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * multi-line text
+ * leading/trailing empty lines (with any spaces/tabs)
+ * leading tabs and spaces
+ * keep indent in multi-line strings
+ * extra characters:
+ * new line \n
+ * tab \t
+ * feed \f
+ * single quote \'
+ * backslash \\
+ */
+public class IndentHelperTest {
+
+
+    @Test
+    public void leadingAndTrailingSpaces() {
+        assertThat(removeIndent("  text  ")).isEqualTo("text");
+    }
+
+    @Test
+    public void multiLineText() {
+        assertThat(removeIndent("text\ntext")).isEqualTo("text\ntext");
+    }
+
+    @Test
+    public void removeLeadingAndTrailingBlankLines() {
+        assertThat(removeIndent("  \n text to believe\ntext to work \n  ")).isEqualTo(" text to believe\ntext to work");
+    }
+
+    @Test
+    public void emptyText() {
+        assertThat(removeIndent("")).isEqualTo("");
+        assertThat(removeIndent("\n")).isEqualTo("");
+    }
+
+    @Test
+    public void moveTextToLeftWithEmptyLineInMiddle() {
+        assertThat(removeIndent("  text    text\n\n  text    text")).isEqualTo("text    text\n\ntext    text");
+        assertThat(removeIndent("  text    text\n\n\n  text")).isEqualTo("text    text\n\n\ntext");
+    }
+
+    @Test
+    public void removeIndentUsingSpace() {
+        assertThat(removeIndent(" text")).isEqualTo("text");
+        assertThat(removeIndent("  text")).isEqualTo("text");
+        assertThat(removeIndent("    text")).isEqualTo("text");
+        assertThat(removeIndent(" \n    text\n  ")).isEqualTo("text");
+        assertThat(removeIndent(" \n    text   \n")).isEqualTo("text");
+    }
+
+    @Test
+    public void textTab() {
+        assertThat(removeIndent("\ttext")).isEqualTo("text");
+        assertThat(removeIndent("\t  text  ")).isEqualTo("text");
+        assertThat(removeIndent("\t\ttext")).isEqualTo("text");
+    }
+    @Test
+    public void multiLineTextTabLines() {
+        assertThat(removeIndent("\ttext\n\ttext")).isEqualTo("text\ntext");
+        assertThat(removeIndent("\t\ttext\n\t\ttext")).isEqualTo("text\ntext");
+        assertThat(removeIndent("text\n\ttext")).isEqualTo("text\n\ttext");
+        assertThat(removeIndent("\ttext\n\t\ttext")).isEqualTo("text\n\ttext");
+        assertThat(removeIndent("\ttext\ntext")).isEqualTo("\ttext\ntext");
+
+    }
+
+    @Test
+    public void multiLineTextTabWithSpaces() {
+        assertThat(removeIndent("text \n\t text")).isEqualTo("text\n\t text");
+        assertThat(removeIndent("\t  text\n\t  text")).isEqualTo("text\ntext");
+        assertThat(removeIndent("  \ttext\n  \ttext")).isEqualTo("text\ntext");
+
+
+    }
+
+    @Test
+    public void multiLineTextTabWithNeededSpaces() {
+        assertThat(removeIndent("\ttext\n\t  text")).isEqualTo("text\n  text");
+        assertThat(removeIndent("\ttext\n\t  text")).isEqualTo("text\n  text");
+        assertThat(removeIndent("\t  text  \ttext\n\t  text")).isEqualTo("text  \ttext\ntext");
+        assertThat(removeIndent("\t  text\n\t  text  \ttext")).isEqualTo("text\ntext  \ttext");
+    }
+
+    @Test
+    public void multiLineTextTabEmptyLineBetween() {
+        assertThat(removeIndent("\ttext\n\n\ttext")).isEqualTo("text\n\ntext");
+
+
+    }
+
+    @Test
+    public void unorderedTabs() {
+        assertThat(removeIndent("  \ttext\n\t   text")).isEqualTo("  \ttext\n\t   text");
+        assertThat(removeIndent("\t  text\n  \ttext ")).isEqualTo("\t  text\n  \ttext");
+        assertThat(removeIndent("\t  text\n  \ttext \t  text\n  \ttext")).isEqualTo("\t  text\n  \ttext \t  text\n  \ttext");
+    }
+}
